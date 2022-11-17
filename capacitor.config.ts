3e728cce3944a7372ec7c463becd85cddbaf12dc
Rennay Dorasamy <rennayd@amazon.com>
2022-11-17 23:48:14 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'liberty_ui',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
